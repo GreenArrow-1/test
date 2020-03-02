213b6cac2fe87947d1db888edb032de45b8c3715
Flash <928677378@qq.com>
2020-03-02 10:43:07 +0800

    测试

---
+++ src/main/java/com/ll/dshare/DshareApplication.java
@@ -0,0 +1,13 @@
+package com.ll.dshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DshareApplication.class, args);
+    }
+
+}
